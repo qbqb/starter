cae5598ae0bdae859b008262aed21a4edb8897e3
qbqb <qbqb@live.ru>
2014-08-15 12:18:18 +0400

    убрал pjax, обновил стили

--- develop/assets/js/init.js
+++ develop/assets/js/init.js
@@ -12,67 +12,6 @@ $(function(){
 
 
 
-    // $('a').pjax('#content');
-
-
-
-
-    // (function(){
-
-    //     var timer1, timer2;
-
-    //     $('a').click(function(e){
-
-    //         e.preventDefault();
-
-    //         if( $(this).hasClass('current') ) return;
-
-    //         clearTimeout(timer1);
-    //         clearTimeout(timer2);
-
-    //         var href = $(this).attr('href');
-
-    //         $('a').removeClass('current');
-    //         $(this).addClass('current');
-
-    //         $('#content').stop().fadeOut(400, 'easeInOutQuad');
-
-    //         $.pjax({
-    //           url: href,
-    //           container: '#pjax-hidden',
-    //            success: function(data){
-
-    //                 timer1 = setTimeout(function(){
-    //                     $('#content').html(data);
-
-
-
-    //                 },410);
-
-    //                 timer2 = setTimeout(function(){
-
-    //                      $('#content').stop().fadeIn(400, 'easeInOutQuad',function(){
-    //                         $('#pjax-hidden').html('');
-
-
-    //                      });
-    //                 },700);
-    //            }
-
-    //         });
-
-
-
-    //     });
-
-
-    // })();
-
-
-
-
-
-
 });
 
 
