e438881a1a06b6edea79566032990206c3b46d40
qbqb <qbqb@live.ru>
2014-10-12 18:10:12 +0400

    edits

---
+++ develop/assets/js/init.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+    $('body').stickTheFooter({offset:-20});
+
+    $('input, textarea').placeholder({
+        color : '#8f8f8f'
+    });
+
+    //$('.dropdown').dropdown();
+
+    //$('.fancybox').fancybox();
+
+
+
+
+
+});
